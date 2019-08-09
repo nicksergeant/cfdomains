1bbf99c1e8168204f4e4ffc31118e50e3b958308
Nick Sergeant <nsergeant@hubspot.com>
2019-08-09 11:40:36 -0400

    Finish up v1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # cfdomains
 
 A simple CLI for checking Cloudflare domains.
+
+1. Add a `.env` file:
+
+```
+CLOUDFLARE_API_KEY=<my-api-key>
+```
+
+2. Run `./index.js`.
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
-const { blue, red, green } = require('chalk');
+require('dotenv').config();
+
+const moment = require('moment');
 const request = require('request-promise');
+const { blue, red, green } = require('chalk');
 
 const apiUrl = endpoint => ({
   url: `https://api.cloudflare.com/client/v4/${endpoint}`,
   headers: {
     'Content-Type': 'application/json',
     'X-Auth-Email': 'nick@flxsoftware.com',
-    'X-Auth-Key': 'ba240ad5e7a8a3e096d4187b32c39106deb2b'
+    'X-Auth-Key': process.env.CLOUDFLARE_API_KEY
   }
 });
 
@@ -18,17 +21,75 @@ const req = async endpoint => {
   });
 };
 
+const colorized = ({ value, condition }) => {
+  return condition ? green(value) : red(value);
+};
+
 const run = async () => {
   const accounts = await req('accounts');
-  const account = accounts.result[0].id;
-  console.log(account);
+  const accountId = accounts.result[0].id;
+  const domains = await req(`accounts/${accountId}/registrar/domains`);
 
-  // Get domains.
-  // List each domain with:
-  // - name
-  // - date of expiration
-  // - auto-renew enabled
-  // - domain lock enabled
+  domains.result.map(d => {
+    const nameLength = d.name.length;
+
+    if (nameLength > 20) {
+      spacing = '\t';
+    } else if (nameLength < 14) {
+      spacing = '\t\t\t';
+    } else {
+      spacing = '\t\t';
+    }
+
+    const registrar = colorized({
+      value: d.current_registrar.substr(0, 10),
+      condition: d.current_registrar === 'Cloudflare'
+    });
+
+    const renew = colorized({
+      value: d.auto_renew || false,
+      condition: Boolean(d.auto_renew)
+    });
+
+    const locked = colorized({
+      value: d.locked || false,
+      condition: Boolean(d.locked)
+    });
+
+    const expires = colorized({
+      value: moment(d.expires_at).format('L'),
+      condition: moment(d.expires_at).isAfter(moment().add(12, 'months'))
+    });
+
+    console.log(
+      `${d.name}  ${spacing}Reg: ${registrar}\t  Renew: ${renew}\tLocked: ${locked}\tExpires: ${expires}`
+    );
+
+    // administrator_contact
+    // auto_renew
+    // available
+    // billing_contact
+    // can_register
+    // cloudflare_dns
+    // cloudflare_registration
+    // contacts_updated_at
+    // current_registrar
+    // ds_records
+    // expires_at
+    // fees
+    // last_transferred_at
+    // locked
+    // name
+    // name_servers
+    // payment_expires_at
+    // pending_transfer
+    // registrant_contact
+    // registry_object_id
+    // registry_statuses
+    // supported_tld
+    // technical_contact
+    // transfer_conditions
+  });
 };
 
 run();
